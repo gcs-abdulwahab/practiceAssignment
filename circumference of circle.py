8522e36bc9dc85ede7c1e452042f55aafa51ca6b
gcs-abdulwahab <100867378+gcs-abdulwahab@users.noreply.github.com>
2022-06-29 10:05:04 +0500

    Update circumference of circle.py
    please read the comments

--- circumference of circle.py
+++ circumference of circle.py
@@ -3,6 +3,8 @@ def circum(radius):
     pi = math.pi
     circumference = 2 * pi*radius**2
     print("Circumference of a circle = ",  circumference)
+    # it should return the circumference 
+    # function name should be descriptive and result variable could be shortcase
 
 
 radius = float(input("Enter radius of a circle "))
